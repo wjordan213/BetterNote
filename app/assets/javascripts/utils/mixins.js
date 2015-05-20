d8659fd94469bab7179b5b1ec15e8901b4a4ed6e
App Academy Student <appacademy@Apps-Mac-mini-19.local>
2015-05-19 21:08:17 -0400

    successfully create mixin and add to AddTitle view

--- app/assets/javascripts/utils/mixins.js
+++ app/assets/javascripts/utils/mixins.js
@@ -12,3 +12,51 @@ BetterNote.Mixins.PaneChanger = {
     $('.sidePane').html(BetterNote._currentPane.render().$el)
   }
 }
+
+BetterNote.Mixins.NoteSubmit = {
+  submit: function(event) {
+    event.preventDefault();
+    var formData = this.$form.serializeJSON();
+    // debugger
+    delete formData.tag;
+    var notebook = BetterNote.notebooks.get(formData.notebook_id);
+
+    this.collection = notebook.notes();
+    this.model.set(formData);
+    if (this.model.get('title') === "") {
+      return;
+    }
+
+    // iterate through this.model.tags(), creating tag_ids
+    this.model.set({tag_ids: []});
+
+
+
+    this.model.tags().each(function(tag) {
+      this.model.get('tag_ids').push(tag.get('id'));
+    }.bind(this))
+
+    // up to here everything looks good. query contains the right array
+
+    var wasNew = this.model.isNew();
+
+    this.model.save({}, {
+      parse: true,
+      success: function() {
+        this.collection.add(this.model, { merge: true });
+        BetterNote.notes.add(this.model, { merge: true });
+
+
+
+        if (wasNew) {
+          Backbone.history.navigate('notes/' + this.model.id + '/edit', {trigger: true});
+          $('.main_input').focus()
+        }
+      }.bind(this),
+
+      failure: function(response) {
+        this.render();
+      }.bind(this)
+    })
+  }
+}

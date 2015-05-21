4f6e9aa485e9df371ee8120504847f929901d6da
harris <harris@aa.io>
2015-05-21 11:25:39 -0400

    debug search by turning collectionViews into an actual collection

--- app/assets/javascripts/views/side_pane/side_pane.js
+++ app/assets/javascripts/views/side_pane/side_pane.js
@@ -1,7 +1,8 @@
 BetterNote.Views.SidePane = Backbone.CompositeView.extend({
 
   initialize: function(options) {
-    this.collectionViews = {};
+    this.collectionViews = [new Backbone.Collection(), {}];
+    this.collectionViews[0].comparator = this.collection.comparator;
     this.type = options.type;
     this.listenTo(this.collection, 'sort', this.addContentViews);
     this.listenTo(this.collection, 'remove', this.removeContentView);
@@ -15,21 +16,13 @@ BetterNote.Views.SidePane = Backbone.CompositeView.extend({
     this.addContentViews();
   },
 
-  // here we have three situations
-  // 1) the model is not in the sidePane's collection
-  //      -early return and move on
-  // 2) the model is in the collection and is not currently rendered on the page.
-  //      -early return and move on
-  // 3) the model is in the collection and is currently rendered on the page
-  //      -remove the corresponding subview, then re-insert it
   removeAndInsert: function(model) {
-    // debugger;
-    if (this.containsModel(model) && this.collectionViews[model.id]) {
+    if (this.containsModel(model) && this.collectionViews[0].contains(model)) {
       console.log('called rmandins');
-      this.collectionViews[model.id].remove();
-      this.collectionViews[model.id] = false;
-
-      // debugger;
+      // this.removeContentView(model);
+      this.collectionViews[0].remove(model);
+      this.collectionViews[1][model.id].remove();
+      delete this.collectionViews[1][model.id];
 
       var subview = new BetterNote.Views.SideContent({ model: model, type: this.type, collection: this.collection });
       this.insertContent(subview);
@@ -61,8 +54,8 @@ BetterNote.Views.SidePane = Backbone.CompositeView.extend({
 
   prependContent: function(content) {
     var subview = new BetterNote.Views.SideContent({ model: content, type: this.type, collection: this.collection });
-    this.collectionViews[content.get('id')] = subview;
-
+    this.collectionViews[1][content.get('id')] = subview;
+    this.collectionViews[0].add(content, {sort: true});
     this.addSubview('.content', subview, true);
   },
 
@@ -85,62 +78,40 @@ BetterNote.Views.SidePane = Backbone.CompositeView.extend({
     // 1) check to see if the title of content matches inputVal
     // debugger;
     if (content.get('title').match(inputVal)) {
-      if (!this.collectionViews[content.get('id')]) {
+      if (!this.collectionViews[0].contains(content)) {
         this.addContentView(content, {insert: true});
       }
-    } else if(this.collectionViews[content.get('id')]) {
+    } else if(this.collectionViews[0].contains(content)) {
       this.removeContentView(content, {insert: true});
     }
   },
 
-  // the bug occurs here or in submit
-
-  // called in two contexts
-    // 1) during search
-    // 2) after insertAndRemove()
-
-    // the bug occurs here, as a result of a submit function that is called
-    // on a function that is not new
   insertContent: function(subview) {
     var inserted = false;
 
-    // iterate, call insertAfter on subview.$el, then add subview to this.subviews() and break out of loop
+    for (var idx in this.collectionViews[0].models) {
+      var model = this.collectionViews[0].models[idx];
+      var contentView = this.collectionViews[1][model.id];
 
-
-    // this may be related to the order of collectionViews. I might have to change collectionViews so that I can sort it the same way as the collection?
-
-
-
-    for (var key in this.collectionViews) {
-      var contentView = this.collectionViews[key];
-      // debugger;
       if (contentView === false) {
         continue;
       }
 
-      // from here forward, we know that contentView is something currently rendered on the page
-
-      // contentView is not however in sorted order!
-      // need to resort contentView after any given edit, so in insert and remove
-
-      // here's the plan: replace contentView with a collection of models? remove the model from this collection when it's corresponding subview is removed.
-
-
-      var result = this.collection.comparator(contentView.model, subview.model);
-      // if result is one, subview is older and contentView is newer, else, subview is newer
+      var result = this.collection.comparator(contentView.model, subview.model) * -1;
       if (result === -1) {
-        // if (subview.model.get('title') === 'created thirdasas') { debugger}
         inserted = true;
-        // debugger;
-        subview.render().$el.insertAfter(contentView.$el);
-        this.collectionViews[subview.model.get('id')] = subview;
+
+        subview.render().$el.insertBefore(contentView.$el);
+        this.collectionViews[1][subview.model.get('id')] = subview;
+        this.collectionViews[0].add(subview.model, {sort: true});
         break;
       }
     }
-      // access this.collection's comparator function, giving it contentView and subView. It should start from newest and work it's way down to oldest. when it finds something older than it, it is inserted either before or after (not sure how the ordering works out)
+
     if (!inserted) {
-      this.collectionViews[subview.model.get('id')] = subview;
-      this.prependContent(subview.model);
+      this.collectionViews[1][subview.model.get('id')] = subview;
+      this.collectionViews[0].add(subview.model, {sort: true});
+      this.addContentView(subview.model);
     } else {
       this.subviews('.content').push(subview);
     }
@@ -158,17 +129,19 @@ BetterNote.Views.SidePane = Backbone.CompositeView.extend({
 
   removeContentView: function(content) {
     this.removeModelSubview('.content', content);
-    this.collectionViews[content.get('id')] = false;
+    this.collectionViews[1][content.get('id')] = false;
+    this.collectionViews[0].remove(content);
   },
 
   addContentView: function(content, options) {
     var subview = new BetterNote.Views.SideContent({ model: content, type: this.type, collection: this.collection });
 
 
+      this.collectionViews[1][content.get('id')] = subview;
+      this.collectionViews[0].add(content, {sort: true});
     if (options && options.insert) {
       this.insertContent(subview);
     } else{
-      this.collectionViews[content.get('id')] = subview;
       this.addSubview('.content', subview);
     }
   },

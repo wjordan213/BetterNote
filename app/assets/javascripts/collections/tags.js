e14eebd35eb9bde9e35b5612fdd32c38c1e2b4c3
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2015-05-20 20:36:01 -0400

    change tags to be sorted by alphabetical order

--- app/assets/javascripts/collections/tags.js
+++ app/assets/javascripts/collections/tags.js
@@ -2,17 +2,17 @@ BetterNote.Collections.Tags = Backbone.Collection.extend({
   url: '/api/tags',
   model: BetterNote.Models.Tag,
   comparator: function(first, second) {
-    var first_updated = first.get('updated_at');
-    var second_updated = second.get('updated_at');
+    var first_updated = first.get('title');
+    var second_updated = second.get('title');
     if (first_updated > second_updated) {
-      return -1;
+      return 1;
     } else if (first_updated === second_updated) {
       return 0;
     } else {
-      return 1;
+      return -1;
     }
   },
-  
+
   getOrFetch: function(id) {
     var tag = this.get(id);
 

090bc41b756b384bbc3a31aad43194bac70b24b8
Harris Jordan <wjordan213@gmail.com>
2015-06-05 12:44:18 -0400

    update home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    root to: 'static_pages#home'
+    root to: 'sessions#new'
 
     get '/app/start', to: 'static_pages#root'
     get "/auth/:provider/callback", to: "sessions#omniauth"
